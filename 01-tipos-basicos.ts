8ecf98846070ad13c2f9acc6051d300eb6d49732
jonathanbarzola <jonabl2102@gmail.com>
2022-11-21 13:20:45 -0500

    basic data types

---
+++ 01-tipos-basicos.ts
@@ -0,0 +1,9 @@
+// TIPOS DE DATOS BÁSICOS
+
+let nombre: string = 'Jonathan';    // STRING
+let hp: number | string = 99;       // NUMBER
+let isAlive: boolean = true;        // BOOLEAN
+
+hp = 'FULL'
+
+console.log(nombre, hp, isAlive);
