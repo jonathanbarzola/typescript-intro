ac78917d9873abe7102952aac096f9cf3c69c0a1
jonathanbarzola <jonabl2102@gmail.com>
2022-11-22 11:17:34 -0500

    TypeScript Classes

---
+++ 08-clases.ts
@@ -0,0 +1,21 @@
+class PersonaNormal {
+
+    constructor(
+        public nombre: string,
+        public direccion: string
+    ){}
+}
+
+class Heroe extends PersonaNormal{
+
+    constructor(
+        public alterEgo: string,
+        public edad: number,
+        public nombreReal: string,
+    ) {
+        super(nombreReal, 'New York, USA');
+    }
+}
+
+const ironman = new Heroe('Ironman', 45, 'Tony Stark');
+console.log(ironman);
