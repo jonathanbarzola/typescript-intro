fc7e8a6a1adb39ca23f2a8e8ee423854a4f3e8ba
jonathanbarzola <jonabl2102@gmail.com>
2022-11-22 09:27:36 -0500

    Destructuring of arguments

---
+++ 06-desestructuracion-funcion.ts
@@ -0,0 +1,32 @@
+interface Producto {
+    descripcion: string;
+    precio: number;
+}
+
+const telefono: Producto = {
+    descripcion: 'Nokia A1',
+    precio: 150
+}
+
+const tableta: Producto = {
+    descripcion: 'iPad Air',
+    precio: 350
+}
+
+function calculaISV( productos: Producto[] ): [number, number] {
+
+    let total = 0;
+
+    productos.forEach( ( { precio } ) => {
+        total += precio;
+    })
+
+    return [ total, total * 0.15 ];
+}
+
+const articulos = [ telefono, tableta ];
+
+const [ total, isv ] = calculaISV( articulos );
+
+console.log('Total: ', total);
+console.log('ISV:', isv);
