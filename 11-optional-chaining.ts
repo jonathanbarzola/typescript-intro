fb45de3ee4f9d580995e66b1ab452355eb9f3d9f
jonathanbarzola <jonabl2102@gmail.com>
2022-11-22 12:23:47 -0500

    TS Optional Chaining

---
+++ 11-optional-chaining.ts
@@ -0,0 +1,22 @@
+interface Pasajero {
+    nombre: string;
+    hijos?: string[];
+}
+
+const pasajero1: Pasajero = {
+    nombre: 'Jonathan',
+}
+
+const pasajero2: Pasajero = {
+    nombre: 'Melissa',
+    hijos: ['Natalia', 'Gabriel']
+}
+
+function imprimeHijos( pasajero: Pasajero): void {
+    
+    const cuantosHijos = pasajero.hijos?.length || 0;
+
+    console.log(cuantosHijos);
+}
+
+imprimeHijos(pasajero1);
